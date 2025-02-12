352cd765ed33e2b3c02e9583a379baa49a1418f7
Manoj Gopu <manojgopu724@gmail.com>
2025-02-12 12:40:20 +0530

    fixed some issues

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -5,7 +5,7 @@ const HeaderComponent = () => {
   return (
     <div className="Header fixed-top">
       <header>
-        <h2 className='d-inline'>MOVIE  INFO</h2>&nbsp;<small>Discover Films Easily....</small>
+        <h2 className='d-inline'>MOVIES  INFO</h2>&nbsp;<small>Discover Films Easily....</small>
       </header>
     </div>
   )
